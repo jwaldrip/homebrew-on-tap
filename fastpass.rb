e0b40f73fadaa45068dc533bc2ba7a4bc13bcf8b
Jason Waldrip <jason@waldrip.net>
2018-12-27 10:28:56 -0700

    Update fastpass.rb

--- fastpass.rb
+++ fastpass.rb
@@ -4,6 +4,7 @@ require "formula"
 class Fastpass < Formula
   LATEST_RELEASE = JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/jwaldrip/fastpass/releases/latest")))
   TAG = LATEST_RELEASE["tag_name"]
+  pp LATEST_RELEASE
   URL = LATEST_RELEASE["assets"].find { |asset| asset["name"].include? "osx" }["browser_download_url"]
 
   version TAG.sub(/^v/, '')
